5e820b55c1b3a5927d9bb55fd40f3e0f58f01673
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2017-10-08 18:10:19 +0200

    Added a CSS flex layout

--- admin/templates/coach-metabox-about.php
+++ admin/templates/coach-metabox-about.php
@@ -1,8 +1,8 @@
-<div class="coach postbox-inside">
-  <div class="coach-picture">
-    <?php Fitness_Planning_Fields::image('fitplan_coach_pic', $fields); ?>
+<div class="coach postbox-inside flex">
+  <div class="coach-picture f33">
+    <?php Fitness_Planning_Fields::image('fitplan_coach_pic', $fields, 'circle'); ?>
   </div>
-  <div class="coach-bio">
+  <div class="coach-bio f66">
     <label for="fitplan_coach_bio"><?php _e('A few words about him', 'fitness-planning'); ?></label>
     <textarea name="fitplan_coach_bio" rows="5"><?php echo $fields['fitplan_coach_bio']; ?></textarea>
   </div>
--- admin/templates/workout-metabox-about.php
+++ admin/templates/workout-metabox-about.php
@@ -1,10 +1,13 @@
-<div class="workout postbox-inside">
-  <div class="workout-description">
+<div class="workout postbox-inside flex">
+  <div class="workout-description f100">
     <label for="fitplan_workout_desc"><?php _e('Description', 'fitness-planning'); ?></label>
     <?php wp_editor( $fitplan_workout_desc, 'fitplan_workout_desc', $settings = Fitness_Planning_Helper::EDITOR_PARAMS); ?>
-
   </div>
-
-
+  <div class="workout-picture f33">
+    <?php Fitness_Planning_Fields::image('fitplan_coach_pic', $fields, 'circle'); ?>
+  </div>
+  <div class="workout-others f66">
+    coucou
+  </div>
 
 </div>
