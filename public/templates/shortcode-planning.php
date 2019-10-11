024f5a1045a2b7e234caebbaacf519e94b41064d
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2019-10-11 09:10:10 +0200

    Fixed bad ordering in mobile view

--- public/templates/shortcode-planning.php
+++ public/templates/shortcode-planning.php
@@ -1,5 +1,5 @@
 <div class="fitplan-wrapper" id="fitplan-<?php echo $attributes['id']; ?>">
-
+ 
   <div class="fitplan-planning fitplan-planning-<?php echo $attributes['id']; ?>" style="background-color: <?php echo $this->datas['fitplan_planning_background_color']; ?>; border-color: <?php echo $this->datas['fitplan_planning_border_color']; ?>;">
     <?php
       foreach($this->datas['weekdays'] as $day):
@@ -7,12 +7,23 @@
     ?>
     <div class="fitplan-planning-day fitplan-planning-day-<?php echo $day['slug']; ?>" style="color: <?php echo $this->datas['fitplan_planning_days_text_color']; ?>; border-color:<?php echo $this->datas['fitplan_planning_border_color']; ?>;">
       <div class="fitplan-planning-title" style="border-color: <?php echo $this->datas['fitplan_planning_border_color']; ?>;"><?php echo $day['name']; ?></div>
-
+ 
       <?php if($this->datas['fitplan_planning_show_morning']): ?>
       <div class="fitplan-planning-morning" style="height: <?php echo $this->datas['planning_height']['morning']; ?>px; border-color: <?php echo $this->datas['fitplan_planning_border_color']; ?>; border-color: <?php echo $this->datas['fitplan_planning_border_color']; ?>44;">
         <?php
           if(isset($this->datas['planning'])):
+            
+            $sortedEntries = array();        
+            
             foreach($this->datas['planning'][$day['slug']] as $id => $entry):
+              if($entry['time'] == "morning"){
+                $sortedEntries[$id] = $entry;
+              }
+            endforeach;
+            
+            array_multisort(array_column($sortedEntries, 'top'), SORT_ASC, SORT_NUMERIC, $sortedEntries);
+            
+            foreach($sortedEntries as $id => $entry):
               if($entry['time'] == "morning"){
                 include 'parts/planning-item.php';
               }
@@ -21,12 +32,23 @@
         ?>
       </div>
       <?php endif; ?>
-
+ 
       <?php if($this->datas['fitplan_planning_show_afternoon']): ?>
       <div class="fitplan-planning-afternoon" style="height: <?php echo $this->datas['planning_height']['afternoon']; ?>px">
         <?php
           if(isset($this->datas['planning'])):
+            
+            $sortedEntries = array();
+            
             foreach($this->datas['planning'][$day['slug']] as $id => $entry):
+              if($entry['time'] == "afternoon"){
+                $sortedEntries[$id] = $entry;
+              }
+            endforeach;
+            
+            array_multisort(array_column($sortedEntries, 'top'), SORT_ASC, SORT_NUMERIC, $sortedEntries);
+            
+            foreach($sortedEntries as $id => $entry):
               if($entry['time'] == "afternoon"){
                 include 'parts/planning-item.php';
               }
@@ -35,7 +57,7 @@
         ?>
       </div>
       <?php endif; ?>
-
+ 
     </div>
     <?php
       endif;
